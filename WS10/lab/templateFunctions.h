99977b68f1f1bdb02140a22dc318a353f8ac641e
RaniaArbash <41278913+RaniaArbash@users.noreply.github.com>
2022-07-29 06:26:50 -0400

    Add files via upload

---
+++ WS10/lab/templateFunctions.h
@@ -0,0 +1,26 @@
+//Name:
+//ID:
+//Email:
+// Date:
+//Section:
+
+#ifndef SDDS_TEMPLATEFUNCTIONS_H_
+#define SDDS_TEMPLATEFUNCTIONS_H_
+#include <iostream>
+#include "Collection.h"
+namespace sdds {
+	//Find with 3 parameters
+	
+
+	//Find with 4 parameters
+	
+
+	//Insertion operator
+	
+
+	//Load Collection
+		
+
+}
+#endif // !SDDS_SEARCH_H_
+
